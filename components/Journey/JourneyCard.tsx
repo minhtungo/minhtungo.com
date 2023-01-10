1d9b7d9fdee364c25b4faa4688d60c93f787fd10
Minh Tu Ngo <loducfc@gmail.com>
2023-01-10 02:59:14 -0500

    More change on Journey Card

--- components/Journey/JourneyCard.tsx
+++ components/Journey/JourneyCard.tsx
@@ -3,24 +3,15 @@ import { motion } from 'framer-motion';
 import { PopUpFromBottom } from '../../lib/FramerMotionVariants';
 import IconHelper from '../common/IconHelper';
 
-
 interface JourneyCardProps {
-  journey: Journey
-
+  journey: Journey;
 }
 
 const JourneyCard = ({ journey }: JourneyCardProps) => {
   return (
-    <motion.div
-      className='mb-4 ml-6 md:ml-8 rounded-lg border-1 dark:border-2 shadow-lg p-4 px-6 bg-transparent-whiter border-transparent-black dark:bg-transparent-black dark:border-transparent-white'
-      initial='hidden'
-      animate='visible'
-      variants={PopUpFromBottom}
-    >
-      <div></div>
-
+    <div className='mb-4 ml-6 md:ml-8 rounded-lg border-1 dark:border-2 shadow-lg p-4 px-6 bg-transparent-whiter border-transparent-black dark:bg-transparent-black dark:border-transparent-white'>
       <span
-        className={`absolute -left-3 flex h-6 w-6 items-center justify-center rounded-full text-black ${journey.color} ring-1 ring-black  dark:ring-gray-700`}
+        className={`absolute -left-3 flex h-6 w-6 items-center justify-center rounded-full text-black ${journey.color} ring-1 ring-transparent-black dark:ring-gray-700`}
       >
         {journey.status ? (
           <>
@@ -43,13 +34,13 @@ const JourneyCard = ({ journey }: JourneyCardProps) => {
           </span>
         ) : null}
       </h3>
-      <time className='text-sm font-normal leading-none text-gray-500 dark:text-gray-500'>
+      <time className='text-sm font-normal leading-none text-gray-500 '>
         {journey.date}
       </time>
       <p className='text-sm text-gray-600 dark:text-gray-400 max-w-none my-1 '>
         {journey.description}
       </p>
-    </motion.div>
+    </div>
   );
 };
 
