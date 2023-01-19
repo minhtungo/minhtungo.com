18ad82939754ceeee2055979024ce25a2c9dbaa7
Minh Tu Ngo <loducfc@gmail.com>
2023-01-19 02:22:42 -0500

    .

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -6,14 +6,14 @@ export default async function handler(
   res: NextApiResponse
 ) {
   // Check for secret to confirm this is a valid request
-  if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
-    return res.status(401).json({ message: 'Invalid token' });
-  }
+  // if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
+  //   return res.status(401).json({ message: 'Invalid token' });
+  // }
 
   try {
     // this should be the actual path not a rewritten path
     // e.g. for "/blog/[slug]" this should be "/blog/post-1"
-    await res.revalidate('/path-to-revalidate');
+    await res.revalidate('/');
     return res.json({ revalidated: true });
   } catch (err) {
     // If there was an error, Next.js will continue
