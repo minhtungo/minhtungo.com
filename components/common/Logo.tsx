22d03d010709f0e502e90135117b96609fe710ea
Minh Tu Ngo <loducfc@gmail.com>
2023-01-23 04:47:36 -0500

    Fix Avatar Responsive

--- components/common/Logo.tsx
+++ components/common/Logo.tsx
@@ -5,13 +5,13 @@ const Logo = ({ className }: { className?: string }) => {
   return (
     <div className='flex items-center gap-3'>
       <span>
-        <div className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-[length:400%_400%] p-[1px] w-full rounded-full animate-border'>
+        <div className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-[length:400%_400%] p-[2px] w-full rounded-full animate-border hidden md:inline-flex'>
           <Image
             src='/static/images/avatar.png'
             alt='Avatar'
             height={50}
             width={50}
-            className='w-8 h-8 rounded-full bg-blue-200 dark:bg-gray-700 '
+            className='w-8 h-8 rounded-full bg-blue-200 dark:bg-gray-700 hidden md:inline-flex'
           />
         </div>
       </span>
