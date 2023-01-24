a356b05121c5ea640b5f9353f05c2bc19df5d5cb
Minh Tu Ngo <loducfc@gmail.com>
2023-01-24 00:00:30 -0500

    Update metadata

--- pages/index.tsx
+++ pages/index.tsx
@@ -54,8 +54,11 @@ export default function Home({
   return (
     <>
       <Head>
-        <title>Minh Tu Ngo</title>
-        <meta name='description' content='Minh Tu Ngo Portfolio' />
+        <title>Minh Tu Ngo Portfolio</title>
+        <meta
+          name='description'
+          content='My personal portfolio to showcase my work. Built with NextJS, Tailwind CSS and Sanity.'
+        />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
