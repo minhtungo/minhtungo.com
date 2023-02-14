d9134513acae1ff3000785cdb4d557e8353bd765
Minh Tu Ngo <loducfc@gmail.com>
2023-02-13 20:37:09 -0500

    Remove react observer library

--- src/components/Project/ProjectImage.tsx
+++ src/components/Project/ProjectImage.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import Image from '@/components/common/Image';
 import classNames from 'classnames';
-import Link from 'next/link';
-
-import { useRef, useCallback, useEffect } from 'react';
-import { useInView } from 'react-intersection-observer';
 
 interface ProjectImageProps {
   src: string;
-  gifSrc: string;
   name: string;
   url: string;
   className?: string;
@@ -19,52 +14,20 @@ const ProjectImage = ({
   name,
   className = '',
   url,
-  gifSrc,
 }: ProjectImageProps) => {
-  const videoRef = useRef();
-  const { ref: inViewRef, inView } = useInView({ threshold: 1 });
-
-  const setRefs = useCallback(
-    (node: any) => {
-      // Ref's from useRef needs to have the node assigned to `current`
-      videoRef.current = node;
-      // Callback refs, like the one from `useInView`, is a function that takes the node as an argument
-      inViewRef(node);
-
-      if (node) {
-        node.addEventListener('click', function () {
-          if (this.paused) {
-            this.play();
-          } else {
-            this.pause();
-          }
-        });
-      }
-    },
-    [inViewRef]
-  );
-
-  useEffect(() => {
-    if (!videoRef || !videoRef.current) {
-      return;
-    }
-
-    if (inView) {
-      videoRef.current.play();
-    } else {
-      videoRef.current.pause();
-    }
-  }, [inView]);
-
+  const videoSrc = `/static/images/projects/${name
+    .toLowerCase()
+    .split(' ')
+    .join('-')}.webm`;
   return (
-    //replace the space in the name variable with a dash
-    <Link
+    <a
       className={classNames(
         'group relative -mt-[30%] w-[95%] shrink-0 overflow-hidden rounded-xl shadow-2xl before:absolute before:inset-0 before:z-10 before:bg-black/20 sm:-mt-4 md:mt-0 md:w-[58%]',
         className
       )}
       href={url}
       target='_blank'
+      rel='noreferrer'
     >
       <Image
         title={name}
@@ -76,22 +39,15 @@ const ProjectImage = ({
       />
 
       <video
-        ref={setRefs}
         playsInline
         autoPlay
         loop
         muted
         className='block h-full w-full lg:hidden lg:group-hover:block'
       >
-        <source
-          src={`/static/images/projects/${name
-            .toLowerCase()
-            .split(' ')
-            .join('-')}.webm`}
-          type='video/webm'
-        />
+        <source src={videoSrc} type='video/webm' />
       </video>
-    </Link>
+    </a>
   );
 };
 
