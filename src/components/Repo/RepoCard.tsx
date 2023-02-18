d6b8ab99a111cf5926d3a58f0615d41d1f52352f
Minh Tu Ngo <loducfc@gmail.com>
2023-02-18 11:36:27 -0500

    fix: library page doesn't show repos

--- src/components/Repo/RepoCard.tsx
+++ src/components/Repo/RepoCard.tsx
@@ -49,7 +49,7 @@ const RepoCard = ({ repo }: RepoProps) => {
             key={`${repo.name}-${tool}`}
           >
             {renderTech(tool, 'w-5 h-5')}
-            <span className='-left-[100%%] absolute -top-7 w-max scale-0 rounded bg-background-light px-2 py-1 text-sm font-medium text-gray-800 shadow transition-all group-hover:scale-100 dark:bg-background-dark dark:text-gray-300'>
+            <span className='absolute -left-[75%] -top-7 mx-auto w-max scale-0 rounded bg-background-light px-2 py-1 text-sm font-medium text-gray-800 shadow transition-all group-hover:scale-100 dark:bg-background-dark dark:text-gray-300'>
               {tool}
             </span>
           </div>
--- src/components/Repo/Repos.tsx
+++ src/components/Repo/Repos.tsx
@@ -12,15 +12,7 @@ interface ReposProps {
   home?: boolean;
 }
 
-const SKIP = 6;
-
 const Repo = ({ repos, home }: ReposProps) => {
-  const [repoList, setRepoList] = useState<Repo[] | undefined>([]);
-
-  useEffect(() => {
-    setRepoList(repos);
-  }, []);
-
   return (
     <section id='repos' className={classNames(home && 'pt-16 lg:pt-20')}>
       <Title
@@ -37,7 +29,7 @@ const Repo = ({ repos, home }: ReposProps) => {
         variants={FadeContainer}
         className='mx-auto grid grid-cols-1 gap-3 md:grid-cols-2 lg:mt-6 lg:grid-cols-3'
       >
-        {repoList?.slice(0, SKIP).map((repo) => (
+        {repos?.map((repo) => (
           <motion.div variants={PopUpFromBottom} key={repo.name}>
             <RepoCard repo={repo} />
           </motion.div>
@@ -46,7 +38,7 @@ const Repo = ({ repos, home }: ReposProps) => {
       {home && (
         <div className='group relative mt-4 text-center transition duration-200 lg:mt-6'>
           <Button href='/library' variant='secondary'>
-            See More Projects{' '}
+            See More Repos{' '}
             <IoMdArrowForward className='ml-2 group-hover:ml-3' />
           </Button>
         </div>
