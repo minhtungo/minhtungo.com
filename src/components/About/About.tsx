1fb4371f4bfbf6f2500ffc8e57e6786c563e6852
Minh Tu Ngo <loducfc@gmail.com>
2023-02-18 23:12:52 -0500

    Improve about page responsive

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -15,14 +15,14 @@ const About = ({ home, className }: { home?: boolean; className?: string }) => {
             : 'Sharing my passions and experiences with the world'
         }
         home={home}
-        className='md:!mb-2 xl:!mb-0'
+        className='lg:!mb-3'
       />
-      <div className='lg:gap-18 flex flex-col items-center justify-center gap-8 md:flex-row-reverse'>
+      <div className='lg:gap-18 flex flex-col items-center justify-center gap-8 lg:flex-row-reverse'>
         <motion.div
           initial='hidden'
           whileInView='visible'
           variants={PopUpFromBottom}
-          className='w-full lg:w-1/3'
+          className='w-full lg:w-1/3 xl:pt-6'
         >
           <ProfileCard />
         </motion.div>
@@ -34,21 +34,22 @@ const About = ({ home, className }: { home?: boolean; className?: string }) => {
               </h3>
               {/* <hr className='mb-3 border-b border-custom-border-black dark:border-transparent-white' /> */}
               <p className='mb-3 text-sm leading-relaxed text-gray-600 dark:text-gray-400 sm:text-base'>
-                From the moment I produced {'"'}Hello World{'"'} in the console
-                of my first application, I knew I was hooked into the world of
-                web development. It has never been {'"'}just a job{'"'} for me,
-                it{"'"}s offered an engaging challenge to continually learn and
-                improve my skills in creating high-quality websites.
+                {
+                  'My journey in web development began with the classic "Hello World" console output, which ignited a deep passion for creating exceptional web experiences. Since then, I have continually strived to learn and get feedback to make myself better and improve.'
+                }
+              </p>
+              <p className='mb-3 text-sm leading-relaxed text-gray-600 dark:text-gray-400 sm:text-base'>
+                As a dedicated and passionate Web Developer, I specialize in
+                using cutting-edge technologies such as React and NextJS to
+                build powerful and scalable web application. I am focused on
+                being a valuable part of any team I am on and I love working
+                with dedicated teams to create amazing web applications to make
+                the internet a better place.
               </p>
               <p className='mb-0 text-sm leading-relaxed text-gray-600 dark:text-gray-400 sm:text-base'>
-                I am a passionate Web Developer with experience building web
-                apps using React, NextJS, TypeScript and NodeJS. I am focused on
-                being a valuable part of any team I am on and believe in the
-                team rising together. I love working with dedicated teams to
-                create amazing web applications to make the internet a better
-                place. Currently, I am looking for an opportunity to
-                continuously learn, grow and make positive contributions to the
-                company.
+                {
+                  "I am constantly looking for new opportunities to learn and grow as a developer. If you're looking for a driven and enthusiastic team player to help create amazing web applications, let's work together!"
+                }
               </p>
             </div>
           </AnimatedDiv>
