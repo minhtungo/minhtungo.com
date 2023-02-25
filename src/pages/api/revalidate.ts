52309c376a1863560d4e133442b553021300a828
Minh Tu Ngo <loducfc@gmail.com>
2023-02-24 23:03:07 -0500

    Check for secret to confirm before revalidate route

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -6,9 +6,9 @@ export default async function handler(
   res: NextApiResponse
 ) {
   // Check for secret to confirm this is a valid request
-  // if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
-  //   return res.status(401).json({ message: 'Invalid token' });
-  // }
+  if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
 
   try {
     // this should be the actual path not a rewritten path
