c326838d1ec706cea0d474d8e5b1d3448925c474
Minh Tu Ngo <loducfc@gmail.com>
2023-03-20 03:45:17 -0400

    style: make the nav items smaller

--- src/components/Header/NavItem.tsx
+++ src/components/Header/NavItem.tsx
@@ -18,8 +18,8 @@ const NavItem = ({ href, label }: NavItemProps) => {
       className={`${
         isActive
           ? 'font-bold text-gray-800 dark:text-gray-200'
-          : 'font-medium text-gray-700 hover:text-gray-900 dark:text-gray-300 hover:dark:text-gray-100'
-      } mx-2.5 hidden rounded-md text-base transition-all md:inline-block`}
+          : ' text-gray-700 hover:text-gray-900 dark:text-gray-300 hover:dark:text-gray-100'
+      } mx-2.5 hidden rounded-md text-sm transition-all md:inline-block`}
     >
       <motion.span className='capitalize' variants={PopUp}>
         {label}
