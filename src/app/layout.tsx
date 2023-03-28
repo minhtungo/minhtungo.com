8b470f14ffa19aa5cde38f012934ad70092084fc
Minh Tu Ngo <loducfc@gmail.com>
2023-03-28 17:25:44 -0400

    chore: remove OnRouteChange component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
           {children}
         </Providers>
         <ToastWrapper />
-        <OnRouteChange />
+        {/* <OnRouteChange /> */}
         <Footer />
       </body>
     </html>
