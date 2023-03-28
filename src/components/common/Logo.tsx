1ab61798152adb020db4a1d3926de2c39e523c76
Minh Tu Ngo <loducfc@gmail.com>
2023-03-28 17:01:30 -0400

    style: change font weight logo name

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -4,9 +4,7 @@ const Logo = ({ className }: { className?: string }) => {
   return (
     <div className='flex items-center justify-center gap-0 md:gap-3'>
       <Avatar width={8} height={8} className='mt-1' />
-      <span className='text-md hidden font-semibold lg:inline'>
-        Minh Tu Ngo
-      </span>
+      <span className='text-md hidden font-medium lg:inline'>Minh Tu Ngo</span>
     </div>
   );
 };
