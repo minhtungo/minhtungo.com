3385b3a1c402d36186cd3f58d5885bde8ece3541
minhtungo <loducfc@gmail.com>
2023-11-12 12:29:37 -0500

    fix 3d model lighting effect

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -4,51 +4,25 @@ import { Text } from '@/components/ui/FramerMotion';
 import { LazyDogModel } from '@/components/Home';
 import { Button, Heading, TypingAnimation, Link } from '@/components/ui';
 
-const CONTENT = [
-  'I Love Bubble Tea.',
-  1000,
-  'I Love Building Things For The Web.',
-  1000,
-  'I Love React.',
-  1000,
-  '',
-];
+const CONTENT = ['I Love Bubble Tea.', 1000, 'I Love Building Things For The Web.', 1000, 'I Love React.', 1000, ''];
 
 const Hero = () => {
   return (
     <div className='h-screen flex-1 gap-12 pt-8 md:flex md:items-center md:justify-between lg:pt-0'>
       {/* Dog Model */}
       <div className='relative z-10 mx-auto mt-4 flex h-[270px] w-[270px] basis-2/5 justify-center sm:h-[360px] sm:w-[360px] md:order-2 md:mx-0 md:mt-[6.6rem] md:h-[400px] md:w-[400px] lg:h-[500px] lg:w-[550px]'>
-        <Image
-          className='absolute top-0 h-auto w-auto animate-pulse sm:-top-1 md:left-0 md:-top-12'
-          src='/static/images/sparkles.svg'
-          alt='sparkles'
-          width={60}
-          height={60}
-        />
-
+        <Image className='absolute top-0 h-auto w-auto animate-pulse sm:-top-1 md:left-0 md:-top-12' src='/static/images/sparkles.svg' alt='sparkles' width={60} height={60} />
         <LazyDogModel />
       </div>
       {/* Hero Text */}
       <div className='relative -mt-14 basis-3/5 text-center md:text-start'>
-        <Image
-          className='absolute -top-4 h-auto w-auto animate-pulse sm:-top-[3rem] sm:left-[10%] md:left-0 md:-top-20 '
-          src='/static/images/sparkles.svg'
-          alt='sparkles'
-          width={60}
-          height={60}
-        />
+        <Image className='absolute -top-4 h-auto w-auto animate-pulse sm:-top-[3rem] sm:left-[10%] md:left-0 md:-top-20 ' src='/static/images/sparkles.svg' alt='sparkles' width={60} height={60} />
 
         <Text className='mb-[5px] pl-[3px]' variants={PopUpFromBottom}>
           Hi, I am
         </Text>
         <Text variants={PopUpFromBottom}>
-          <Heading
-            variant='gradient'
-            size='lg'
-            weight='bold'
-            className='primary-gradient inline pb-2'
-          >
+          <Heading variant='gradient' size='lg' weight='bold' className='primary-gradient inline pb-2'>
             Minh Tu Ngo.
           </Heading>
           <span className='wave text-2xl md:text-4xl lg:text-5xl'>👋</span>
@@ -56,15 +30,9 @@ const Hero = () => {
         <div className='text-md mt-3 font-medium dark:text-neutral-300 md:mt-4 md:text-base lg:ml-[2px] lg:text-lg'>
           <TypingAnimation content={CONTENT} />
         </div>
-        <Text
-          variants={PopUpFromBottom}
-          className='lg:text-md mt-4 text-sm dark:text-neutral-400 md:text-base lg:ml-[1px] lg:mt-3'
-        >
+        <Text variants={PopUpFromBottom} className='lg:text-md mt-4 text-sm dark:text-neutral-400 md:text-base lg:ml-[1px] lg:mt-3'>
           {"Don't forget to sign my"}{' '}
-          <Link
-            href='/guestbook'
-            className='primary-gradient bg-clip-text text-transparent'
-          >
+          <Link href='/guestbook' className='primary-gradient bg-clip-text text-transparent'>
             guestbook
           </Link>
           !
--- src/components/Home/Model/DogModel.tsx
+++ src/components/Home/Model/DogModel.tsx
@@ -5,12 +5,8 @@ import { OrbitControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { Dog } from './Dog';
 import { useState, useEffect } from 'react';
-interface DogModelTypes {
-  className?: string;
-}
 
-const DogModel = ({ className }: DogModelTypes) => {
-  const [autoRotate, setAutoRotate] = useState(false);
+const DogModel = () => {
   const [autoRotateSpeed, setAutoRotateSpeed] = useState(0.01);
 
   useEffect(() => {
@@ -21,24 +17,15 @@ const DogModel = ({ className }: DogModelTypes) => {
     }, 800);
   }, []);
   return (
-    <Canvas
-      camera={{ position: [15, 8, 28], fov: 22 }}
-      style={{ width: '100%', height: '100%' }}
-      className='!absolute left-0 md:left-[10px] lg:left-[70px] xl:left-[95px]'
-    >
-      <pointLight position={[5, 5, 5]} intensity={0.5} />
-      <pointLight position={[-3, -3, 2]} intensity={0.5} />
-      <ambientLight position={[5, 5, 5]} intensity={0.5} />
-      <directionalLight intensity={0.2} />
+    <Canvas camera={{ position: [15, 8, 28], fov: 22 }} style={{ width: '100%', height: '100%' }} className='!absolute left-0 md:left-[10px] lg:left-[70px] xl:left-[95px]'>
+      <ambientLight intensity={1} />
+      <pointLight position={[10, 10, 10]} intensity={1} />
+      <pointLight position={[-10, -10, 10]} intensity={1} />
+      <directionalLight position={[0, 5, 5]} intensity={0.8} />
       <Suspense fallback={null}>
         <Dog />
       </Suspense>
-      <OrbitControls
-        autoRotate
-        autoRotateSpeed={autoRotateSpeed}
-        enablePan={false}
-        enableZoom={false}
-      />
+      <OrbitControls autoRotate autoRotateSpeed={autoRotateSpeed} enablePan={false} enableZoom={false} />
     </Canvas>
   );
 };
