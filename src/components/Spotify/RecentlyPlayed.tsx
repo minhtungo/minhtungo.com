b0d3e2c5a2f66e0db8c30d53481ee325909d6219
minhtungo <loducfc@gmail.com>
2024-01-13 21:42:09 -0500

    fix recently played songs card overflow

--- src/components/Spotify/RecentlyPlayed.tsx
+++ src/components/Spotify/RecentlyPlayed.tsx
@@ -1,6 +1,4 @@
 import { Heading, Image, Link } from '@/components/ui';
-import { Div } from '@/components/ui/FramerMotion';
-import { FadeContainer } from '@/lib/framerVariants';
 import { getRecentlyPlayed, normalizeRecentlyPlayed } from '@/lib/spotify';
 
 const fetchRecentlyPlayedSongs = async () => {
@@ -24,22 +22,22 @@ const RecentlyPlayed = async () => {
         <Heading className='mb-2 md:mb-6' as='h3'>
           Recently Played Songs
         </Heading>
-        <Div variants={FadeContainer} className='horizontal-scrollbar flex items-center gap-2 overflow-x-scroll md:gap-2 lg:overflow-hidden xl:gap-0'>
+        <div className='horizontal-scrollbar flex items-center gap-2 overflow-x-scroll xl:gap-3 lg:overflow-x-visible pb-[2px]'>
           {songs?.map((song, index) => (
-            <Link className='group relative mx-auto w-full cursor-pointer text-white/80 shadow-md transition duration-200 ease-out hover:scale-[1.025] hover:text-gray-50 overflow-hidden rounded-sm' href={song?.url} key={index}>
-              <div className='relative h-[360px] w-[260px] overflow-hidden shadow-lg'>
-                <Image src={song?.thumbnail} alt={song?.title} width={400} height={400} className='absolute inset-0 h-full w-full object-cover brightness-[.55] group-hover:brightness-[.85]' />
+            <Link className='group relative mx-auto w-full cursor-pointer text-white/80 shadow-md hover:text-gray-50 rounded-md' href={song?.url} key={index}>
+              <div className='relative h-[360px] min-w-[240px] w-full'>
+                <Image src={song?.thumbnail} alt={song?.title} width={400} height={400} className='absolute inset-0 h-full w-full object-cover min-w-[240px] brightness-[.45] group-hover:brightness-[.75] transition duration-300 ease-in-out rounded-md' />
               </div>
 
               <div className='absolute inset-x-0 bottom-5 ml-3 flex items-center space-x-3'>
                 <div className='w-[236px] break-words text-[15px]'>
-                  <h4 className=' font-bold'>{song?.title}</h4>
+                  <h4 className='font-bold'>{song?.title}</h4>
                   <h6>{song?.artist}</h6>
                 </div>
               </div>
             </Link>
           ))}
-        </Div>
+        </div>
       </div>
     )
   );
