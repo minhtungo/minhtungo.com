5aa3d84c2bc43f8f949c8e7383f92d1335d2f58d
minhtungo <loducfc@gmail.com>
2024-07-18 23:11:24 -0400

    build: add drizzle

---
+++ src/auth/index.ts
@@ -0,0 +1,15 @@
+import db from '@/db';
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
+import NextAuth from 'next-auth';
+import Github from 'next-auth/providers/github';
+import Google from 'next-auth/providers/google';
+
+export const config = {
+  adapter: DrizzleAdapter(db),
+  providers: [Google, Github],
+  pages: {
+    signIn: '/login',
+  },
+};
+
+export const { signIn, signOut, handlers, auth } = NextAuth(config);
