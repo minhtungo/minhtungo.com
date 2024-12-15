4c478528bfc939b58258a149dfd675cb1567656f
minhtungo <loducfc@gmail.com>
2024-12-15 18:34:03 -0500

    feat: add react compiler

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,9 @@
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  experimental: {
+    reactCompiler: true,
+  },
   reactStrictMode: true,
   typescript: {
     ignoreBuildErrors: true,
